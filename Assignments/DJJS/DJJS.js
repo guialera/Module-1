eae523c064028a3f1f0cd7f5a7c9b36d1d9d44d7
Alex Ramirez <guialera@gmail.com>
2020-08-29 20:35:38 -0700

    Committing partially completed DJJS assignment 8/29/20

---
+++ Assignments/DJJS/DJJS.js
@@ -0,0 +1,25 @@
+
+var square = document.getElementById("square")
+square.addEventListener("mousedown", function() {
+    square.style.backgroundColor = "red";
+})
+
+var square = document.getElementById("square")
+square.addEventListener("mouseup", function() {
+    square.style.backgroundColor = "yellow";
+})
+
+var square = document.getElementById("square")
+square.addEventListener("mouseover", function() {
+    square.style.backgroundColor = "blue";
+})
+
+var square = document.getElementById("square")
+square.addEventListener("dblclick", function() {
+    square.style.backgroundColor = "green";
+})
+
+var square = document.getElementById("square")
+square.addEventListener("scroll", function() {
+    square.style.backgroundColor = "orange";
+})

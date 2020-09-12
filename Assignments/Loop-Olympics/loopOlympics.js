79603b73f27986809988f51bdd8613e4de6f2360
Alex Ramirez <guialera@gmail.com>
2020-09-12 09:10:49 -0700

    Committing completed Loop Olympics assignment with uncompleted #4 gold medal challenge 9/12/20

--- Assignments/Loop-Olympics/loopOlympics.js
+++ Assignments/Loop-Olympics/loopOlympics.js
@@ -40,15 +40,19 @@ for (var i = 0; i <= 100; i++) {
     }
 }
 
-//3 Not Complete
+//3
 
 var fruitTwo = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
 
-for (var i = 0; i < fruitTwo.length; i++) {
+var newFruitTwo = []
+
+for (var i = 0; i < 6; i++) {
     if (i % 2 === 0) {
-        console.log(fruitTwo[i])
+        newFruitTwo.push(fruitTwo[i])
     }
 }
+
+console.log(newFruitTwo)
 
 //Silver Medal
 
@@ -77,4 +81,85 @@ for (var i = 0; i < peopleArray.length; i++) {
     console.log(peopleArray[i].name)
 }
 
-//2 Not Complete
+//2
+
+var peopleArrayNames = []
+
+for (var i = 0; i < 4; i++) {
+    peopleArrayNames.push(peopleArray[i].name)
+}
+
+console.log(peopleArrayNames)
+
+var peopleArrayOccupations = []
+
+for (var i = 0; i < 4; i++) {
+    peopleArrayOccupations.push(peopleArray[i].occupation)
+}
+
+console.log(peopleArrayOccupations)
+
+//3
+
+var everyOtherPeopleNames = []
+
+for (var i = 0; i < 4; i++) {
+    if (i % 2 === 0) {
+        everyOtherPeopleNames.push(peopleArray[i].name)
+    }
+}
+
+console.log(everyOtherPeopleNames)
+
+var everyOtherPeopleOccupations = []
+
+for (var i = 0; i < 4; i++) {
+    if (i % 2 != 0) {
+        everyOtherPeopleOccupations.push(peopleArray[i].occupation)
+    }
+}
+
+console.log(everyOtherPeopleOccupations)
+
+//Gold Medal
+
+//1
+
+var nestedLoopsOne = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+
+]
+
+for (var i = 0; i < nestedLoopsOne.length; i++) {
+    console.log(nestedLoopsOne[i])
+}
+
+//2
+
+var nestedLoopsTwo = [
+    [0, 0, 0],
+    [1, 1, 1],
+    [2, 2, 2]
+
+]
+
+for (var i = 0; i < nestedLoopsTwo.length; i++) {
+    console.log(nestedLoopsTwo[i])
+}
+
+//3
+
+var nestedLoopsThree = [
+    [0, 1, 2],
+    [0, 1, 2],
+    [0, 1, 2]
+
+]
+
+for (var i = 0; i < nestedLoopsThree.length; i++) {
+    console.log(nestedLoopsThree[i])
+}
+
+//4

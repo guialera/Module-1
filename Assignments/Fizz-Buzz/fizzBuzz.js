361596461d0406b3c2aa3585eefc92b780e0d052
Alex Ramirez <guialera@gmail.com>
2020-09-19 11:01:44 -0700

    Commtting an updated version of completed Fizz Buzz assignment with edited Optional Coding Challenge 9/19/2020

--- Assignments/Fizz-Buzz/fizzBuzz.js
+++ Assignments/Fizz-Buzz/fizzBuzz.js
@@ -12,47 +12,53 @@ for (var i = 1; i <= 100; i++) {
 
 //Optional Coding Challenge
 
-var fizzBuzzArr = []
-var fizzArr = []
-var buzzArr = []
-var numberArr = []
+function totalCount() {
 
-for (var i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        fizzBuzzArr.push(i)
-    } else if (i % 3 === 0) {
-        fizzArr.push(i)
-    } else if (i % 5 === 0) {
-        buzzArr.push(i)
-    } else {
-        numberArr.push(i)
+    var fizzBuzzArr = []
+    var fizzArr = []
+    var buzzArr = []
+    var numberArr = []
+
+    for (var i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            fizzBuzzArr.push(i)
+        } else if (i % 3 === 0) {
+            fizzArr.push(i)
+        } else if (i % 5 === 0) {
+            buzzArr.push(i)
+        } else {
+            numberArr.push(i)
+        }
     }
+
+    var fizzBuzzCountArr = []
+
+    var fizzBuzzArrCount = fizzBuzzArr.length
+    fizzBuzzCountArr.push("fizzbuzz: " + fizzBuzzArrCount)
+
+    var fizzArrCount = fizzArr.length
+    fizzBuzzCountArr.push("fizz: " + fizzArrCount)
+
+    var buzzArrCount = buzzArr.length
+    fizzBuzzCountArr.push("buzz: " + buzzArrCount)
+
+    var count = {
+        fizzbuzz: [],
+        fizz: [],
+        buzz: []
+    }
+
+    count.fizzbuzz.push(fizzBuzzArrCount)
+    count.fizz.push(fizzArrCount)
+    count.buzz.push(buzzArrCount)
+
+    console.log(count)
+
+    console.log(fizzBuzzCountArr)
+
 }
 
-var fizzBuzzCountArr = []
-
-var fizzBuzzArrCount = fizzBuzzArr.length
-fizzBuzzCountArr.push("fizzbuzz: " + fizzBuzzArrCount)
-
-var fizzArrCount = fizzArr.length
-fizzBuzzCountArr.push("fizz: " + fizzArrCount)
-
-var buzzArrCount = buzzArr.length
-fizzBuzzCountArr.push("buzz: " + buzzArrCount)
-
-var count = {
-    fizzbuzz: [],
-    fizz: [],
-    buzz: []
-}
-
-count.fizzbuzz.push(fizzBuzzArrCount)
-count.fizz.push(fizzArrCount)
-count.buzz.push(buzzArrCount)
-
-console.log(count)
-
-console.log(fizzBuzzCountArr)
+totalCount(i)
 
 //var numberArrCount = numberArr.length
 //fizzBuzzCountArr.push(numberArrCount)

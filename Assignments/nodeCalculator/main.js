c15078a03bdaa03735073013a094b1a1b5f60779
Alex Ramirez <guialera@gmail.com>
2020-09-25 13:10:30 -0700

    Committing an updated version of completed nodeCalculator assignment with added add, sub, mul, and div functions 9/25/2020

--- Assignments/nodeCalculator/main.js
+++ Assignments/nodeCalculator/main.js
@@ -1,23 +1,36 @@
 var readlineSync = require("readline-sync");
 
-var num1 = readlineSync.question("Please enter your first number! ");
+var num1 = Number(readlineSync.questionInt("Please enter your first number! "));
 console.log(num1);
 
-var num2 = readlineSync.question("Please enter your second number! ");
+var num2 = Number(readlineSync.question("Please enter your second number! "));
 console.log(num2);
 
 var variable = readlineSync.question("Please enter the operation to perform: add, sub, mul, or div! ");
 console.log(variable);
 
-var x = num1;
-var y = num2;
-
 if (variable === "add") {
-    console.log("The result is",(Number(x) + Number(y))); 
+    console.log("The result is", add(num1, num2)); 
 } else if(variable === "sub" ) {
-    console.log("The result is",(Number(y) - Number(x)));
+    console.log("The result is", sub(num1, num2));
 } else if (variable === "mul") {
-    console.log("The result is",(Number(x) * Number (y)));
+    console.log("The result is", mul(num1, num2));
 } else if (variable == "div") {
-    console.log("The result is",(Number (x) / Number (y)));
+    console.log("The result is", div(num1, num2));
+}
+
+function add(num1, num2){
+    return num1 + num2
+}
+
+function sub(num1, num2){
+    return num1 - num2
+}
+
+function mul(num1, num2){
+    return num1 * num2
+}
+
+function div(num1, num2){
+    return num1 / num2
 }
